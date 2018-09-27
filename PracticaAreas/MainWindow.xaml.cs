6cb19bc712d0d4f7cfa181cb7c052a0d7dad010d
david arturo preciado osuna <arturo_preciado@live.com.mx>
2018-09-27 08:47:18 -0700

    se puso mas loco( se agrego combo box)

--- PracticaAreas/MainWindow.xaml.cs
+++ PracticaAreas/MainWindow.xaml.cs
@@ -27,34 +27,76 @@ namespace PracticaAreas
 
         private void btnUnoCR_Click(object sender, RoutedEventArgs e)
         {
-            int aux = int.Parse(tbxBaseR.Text);
+            /*int aux = int.Parse(tbxBaseR.Text);
             int aux2 = int.Parse(tbxAlturaR.Text);
             int aux3 = aux*aux2;
-            tbkAreaR.Text = aux3.ToString();
+            tbkAreaR.Text = aux3.ToString();*/
         }
 
         private void btnAreaTri_Click(object sender, RoutedEventArgs e)
         {
-            int aux = int.Parse(tbxBaseTri.Text);
+            /*int aux = int.Parse(tbxBaseTri.Text);
             int aux2 = int.Parse(tbxAlturaTri.Text);
             int aux3 = (aux * aux2)/2;
-            tbkAreaTri.Text = aux3.ToString();
+            tbkAreaTri.Text = aux3.ToString();*/
         }
 
         private void btnAreaC_Click(object sender, RoutedEventArgs e)
         {
-            double aux = double.Parse(tbxRadioC.Text);
+            /*double aux = double.Parse(tbxRadioC.Text);
             aux = (aux*aux) * 3.14;
-            tbkAreaC.Text = aux.ToString();
+            tbkAreaC.Text = aux.ToString();*/
         }
 
         private void btnAreaTra_Click(object sender, RoutedEventArgs e)
         {
-            int aux = int.Parse(tbxBaseMT.Text);
+           /* int aux = int.Parse(tbxBaseMT.Text);
             int aux2 = int.Parse(tbxBaseMrT.Text);
             int aux3 = int.Parse(tbxAlturaTra.Text);
             int aux4 = (aux+aux2)/2*aux3;
-            tbkAreaTra.Text = aux4.ToString();
+            tbkAreaTra.Text = aux4.ToString();*/
+        }
+
+        private void cbTipoFigura_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            panelConfiguracion.Children.Clear();
+            switch (cbTipoFigura.SelectedIndex)
+            {
+                case 0:  //Rectangulo
+                    panelConfiguracion.Children.Add(new ControlAreaRectangulo());
+                    break;
+                case 1:  //Triangulo
+                    panelConfiguracion.Children.Add(new ControlAreaTriangulo());
+                    break;
+                case 2:  //Circulo
+                    panelConfiguracion.Children.Add(new ControlAreaCirculo());
+                    break;
+                case 3:  //Trapecio
+                    panelConfiguracion.Children.Add(new ControlAreaTrapecio());
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            double area = 0.0;
+            switch (cbTipoFigura.SelectedIndex)
+            {
+                case 0: //Rectangulo
+                    break;
+                case 1: //Triangulo
+                    break;
+                case 2: //Circulo
+                    break;
+                case 3: //Trapecio
+                    break;
+                default:
+                    break;
+
+            }
+            lblResultadoArea.Text = area.ToString();
         }
     }
 }
